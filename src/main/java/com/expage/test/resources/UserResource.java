3446c67a27f58c06ae74adf3a3674913caa68eca
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-24 12:22:58 -0300

    feat: User entity and resource

---
+++ src/main/java/com/expage/test/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.expage.test.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.expage.test.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "John", "john@gmail.com", "997486653", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
