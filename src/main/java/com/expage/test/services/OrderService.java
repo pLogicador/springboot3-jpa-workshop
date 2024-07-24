6204b5bef3cbfb974803fea15c3e892fe558e801
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-24 20:34:17 -0300

    feat: Order, Instant, ISO 8601 - PART2

---
+++ src/main/java/com/expage/test/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.expage.test.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.expage.test.entities.Order;
+import com.expage.test.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
