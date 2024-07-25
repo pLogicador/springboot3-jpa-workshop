d992f585523df4a8de38ed35464286837cd052a2
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-07-25 05:18:16 -0300

    feat: Payment, one-to-one association

--- src/main/java/com/expage/test/config/TestConfig.java
+++ src/main/java/com/expage/test/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.expage.test.entities.Category;
 import com.expage.test.entities.Order;
 import com.expage.test.entities.OrderItem;
+import com.expage.test.entities.Payment;
 import com.expage.test.entities.Product;
 import com.expage.test.entities.User;
 import com.expage.test.entities.enums.OrderStatus;
@@ -81,6 +82,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
+		
 	}
 	
 	
